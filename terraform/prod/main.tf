3a0b3e323576ec7a2dc33c74fed93a6e624a4427
Oleg N <n.ol@mail.ru>
2020-04-20 11:18:15 +0500

    Ex.09 для проверки. Terraform modules. Исправил переменные для travis

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,6 +1,6 @@
 terraform {
   # Версия terraform
-  required_version = "0.12.19"
+  required_version = " ~> 0.12.19"
 }
 provider "google" {
   # Версия провайдера
@@ -12,16 +12,20 @@ provider "google" {
 module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
+  private_key_path = var.private_key_path
   zone            = var.zone
   app_disk_image  = var.app_disk_image
+  reddit_internal_ip = module.db.db_internal_ip
+  provision_enabled = true
 }
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
+  private_key_path = var.private_key_path
   zone            = var.zone
   db_disk_image   = var.db_disk_image
+  provision_enabled = true
 }
 module "vpc" {
-  source          = "../modules/vpc"
-  source_ranges = ["5.189.45.90/32"]
+  source = "../modules/vpc"
 }
--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -2,6 +2,10 @@ output "app_external_ip" {
   value = module.app.app_external_ip
 }
 
-#output "lb_external_ip" {
-#  value = google_compute_global_forwarding_rule.default.ip_address
-#}
+output "db_external_ip" {
+  value = module.db.db_external_ip
+}
+
+output "db_internal_ip" {
+  value = module.db.db_internal_ip
+}
